bb0b4a4d804b183a3a539624e2136e8417cbb0e8
JanZinch <jan.zinchanka@mail.ru>
2021-10-24 19:13:08 +0300

    world_generator

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -4,6 +4,40 @@ using UnityEngine;
 
 public class WorldGenerator : MonoBehaviour
 {
+
+    [SerializeField] private int _pooledPartsCount = 6;
+
+    [SerializeField] private List<GameObject> _tubePartsPrefs = null;
+    [SerializeField] private Queue<GameObject> _tubePartsPool = null;
+
+
+    private GameObject GetRandomPart() {
+
+        return _tubePartsPrefs[Random.Range(0, _tubePartsPrefs.Count)];    
+    }
+
+    private void Awake()
+    {
+        for (int i = 0; i < _pooledPartsCount; i++) {
+
+            _tubePartsPool.Enqueue(GetRandomPart());
+        }
+    }
+
+
+    public void Next() {
+
+        _tubePartsPool.Dequeue();
+        _tubePartsPool.Enqueue(GetRandomPart());
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
