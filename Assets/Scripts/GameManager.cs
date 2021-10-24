672f29155879e789a520052586064ed17d0f175a
JanZinch <jan.zinchanka@mail.ru>
2021-10-24 17:11:28 +0300

    test2

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; set; } = null;
+
+    private void Awake()
+    {
+        
+    }
+}
