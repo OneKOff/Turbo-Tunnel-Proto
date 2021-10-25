d5a5d5447a3de283ffbbd4d1e952e548f4123c30
JanZinch <jan.zinchanka@mail.ru>
2021-10-25 19:55:39 +0300

    fix

--- Assets/Scripts/AI/HelicalActor.cs
+++ Assets/Scripts/AI/HelicalActor.cs
@@ -9,25 +9,10 @@ namespace AI
         [SerializeField] protected float _angularSpeed = default;
         [SerializeField] protected Transform _centerOfMass = null;
 
-        protected override void SetSpeed()
-        {
-            base.SetSpeed();
-
-            //_rigidBody.a
-
-            //_rigidBody.centerOfMass = _centerOfMass.position;
-            //_rigidBody.angularVelocity = new Vector3(0.0f, 0.0f, _angularSpeed);
-
-            transform.RotateAround(_centerOfMass.position, Vector3.right, _angularSpeed * Time.deltaTime);
-            _angularSpeed += 10.0f;
-
-        }
-
         private void Update()
         {
-            //_centerOfMass.position = new Vector3(_centerOfMass.position.x, _centerOfMass.position.y, transform.position.z); 
+            _centerOfMass.Rotate(new Vector3(0.0f, 0.0f, _angularSpeed * Time.deltaTime), Space.Self);
         }
-
 
 
     }
